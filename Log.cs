e45c2f73a7ef06230b47900eeca441d956ced0d1
Jeff Hamm <jeff.hamm@gmail.com>
2010-11-26 21:22:59 +0000

    Several bugfixes.

---
+++ Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CharacterBuilderLoader
+{
+    public class Log
+    {
+        public static bool VerboseMode { get; set; }
+        public static void Debug(string msg)
+        {
+            if(VerboseMode)
+                Console.WriteLine("Debug: " + msg);
+        }
+    }
+}
