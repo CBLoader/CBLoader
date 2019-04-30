2b390dc94a1ebc9f7eb0e444b6f86ea9babdf7cb
Lymia Aluysia <lymia@lymiahugs.com>
2019-04-30 04:40:53 -0500

    Reenable link remover.

--- CBLoader/ProcessLauncher.cs
+++ CBLoader/ProcessLauncher.cs
@@ -270,8 +270,8 @@ namespace CBLoader
             Log.Debug("   - Injecting CBLoader changelog.");
             InjectChangelog(assembly, changelog);
 
-            //Log.Debug("   - Removing D&D Compendium links.");
-            //RemoveCompendiumLinks(assembly);
+            Log.Debug("   - Removing D&D Compendium links.");
+            RemoveCompendiumLinks(assembly);
 
             AddOverride(callback, assembly, true);
         }
