596466f1033d0021b1313a4ff40e141f607ea326
Katelyn Gigante <clockwork.singularity@gmail.com>
2020-05-07 17:50:04 +1000

    Bump version number to 1.4.5 beta

--- CBLoader/Properties/AssemblyInfo.cs
+++ CBLoader/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A homebrew rules loader for the D&D Insider Character Builder")]
 [assembly: AssemblyProduct("CBLoader")]
 [assembly: AssemblyCopyright("Released under the GNU General Public License v2.0")]
-[assembly: AssemblyVersion("1.4.4.*")]
+[assembly: AssemblyVersion("1.4.5.*")]
 
 [assembly: ComVisible(false)]
 
